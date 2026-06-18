b576dcfbe0631d0f027332000613987b15eba7c8
Nathan Oketch <nobuyanzi07@gmail.com>
2026-06-18 14:57:45 +0300

    learnt string interpolation

---
+++ JS/Intro/templateString.js
@@ -0,0 +1,13 @@
+let firstName='John'
+let secondName='GPT'
+let age=75
+let address='Jupiter'
+
+let userDetails=firstName+ " " +
+secondName+" is "+age+" years old "+ "he stays in "+address
+console.log(userDetails)
+console.log ("user details is",typeof userDetails)
+
+let userDetails2=`${firstName} ${secondName} is ${age} years old and stays at ${address}`;
+console.log (userDetails2)
+
