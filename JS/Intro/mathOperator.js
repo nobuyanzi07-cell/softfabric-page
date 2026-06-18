de6b9a37e9f4f8c8ae282e12eff08666b007505f
Nathan Oketch <nobuyanzi07@gmail.com>
2026-06-18 19:06:49 +0300

    smore stuff learnt

---
+++ JS/Intro/mathOperator.js
@@ -0,0 +1,26 @@
+let numb1=23
+console.log(`numb1 ${numb1} ${20+20+20} its type is ${typeof numb1}`)
+numb1=40
+console.log(`numb1 ${numb1} its type is ${typeof numb1}`)
+
+let numb2=60
+console.log(`numb2 ${numb2} its type is ${typeof numb2}`)
+let numb3=numb1+numb2+10
+console.log(`numb3 ${numb3} its type is ${typeof numb3}`)
+
+let numb4=numb1*numb2
+console.log(`numb4 ${numb4} its type is ${typeof numb4}`)
+
+let numb5=2**3
+console.log(`numb5 ${numb5} its type is ${typeof numb5}`)
+
+let numb6=numb3/numb2
+console.log(`numb6 ${numb6} its type is ${typeof numb6}`)
+
+let numb7=Math.pow(2,3)
+console.log(`numb7 ${numb7} its type is ${typeof numb7}`)
+
+let numb8=34
+let numb9=98
+let modNumb=numb8%numb9
+console.log(`Module Number is ${modNumb}, its data type is ${typeof modNumb}`)
