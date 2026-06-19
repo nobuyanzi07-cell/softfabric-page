5595f5f4881154b4d50d37fda9f8b5e48701787e
Nathan Oketch <nobuyanzi07@gmail.com>
2026-06-19 10:18:58 +0300

    more learnet

---
+++ JS/Intro/comparisonOperator.js
@@ -0,0 +1,19 @@
+let numb1="2321tc"
+let numb2=23
+let strictEqual=numb1===numb2
+console.log(`numb1=${numb1}===numb2=${numb2}
+    Ans ${strictEqual} its type is ${typeof strictEqual}`)
+
+let nonStrictEquality=numb1==numb2
+console.log(`numb1=${numb1}===numb2=${numb2}
+    Ans ${nonStrictEquality} its type is ${typeof nonStrictEquality}`)
+
+let val1="hello"
+let val2="How are you"
+let strictEquality=val1!==val2
+console.log(`val1=${val1}!==val2=${val2}
+    Ans ${strictInquality} its type is ${typeof strictInequality}`)
+
+let Inequality=val1!=val2
+console.log(`val1=${val1}!=val2=${val2}
+    Ans ${Inequality} its type is ${typeof Inequality}`)
