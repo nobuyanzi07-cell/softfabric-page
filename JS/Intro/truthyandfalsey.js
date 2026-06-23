44aaa808a7890ac31206874adcea11bb509d32b3
Nathan Oketch <nobuyanzi07@gmail.com>
2026-06-23 17:10:25 +0300

    learnt truthy and falsey operator

---
+++ JS/Intro/truthyandfalsey.js
@@ -0,0 +1,9 @@
+let userName1="John Mwangi"
+let userName2=0
+
+let activeUserName=userName1 || userName2
+
+console.log(activeUserName)
+
+let bothUserNameOk=userName1&&userName2
+console.log(bothUserNameOk)
