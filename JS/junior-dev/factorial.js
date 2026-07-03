f1d605cbf0b5cf1c6761938bd1b239e3adecb641
Nathan Oketch <nobuyanzi07@gmail.com>
2026-07-03 10:51:06 +0300

    factorial task completion

---
+++ JS/junior-dev/factorial.js
@@ -0,0 +1,14 @@
+function factorial(number) {
+    // number has to be greater than 1
+    if (number < 1) {
+        return "Please enter a number greater than 0"
+    }
+    // base special case
+    if (number === 1) {
+        return 1
+    }
+    // recursive case; number * factorial(number-1)
+    return number * factorial(number - 1)
+}
+
+console.log(factorial(5)) // 120
