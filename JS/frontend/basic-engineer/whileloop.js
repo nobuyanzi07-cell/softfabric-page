bcbe5ca7ba70ed6cb45fb449ef7579bdaf543577
Nathan Oketch <nobuyanzi07@gmail.com>
2026-07-07 12:32:44 +0300

    finished gib=ven assignment

--- JS/frontend/basic-engineer/whileloop.js
+++ JS/frontend/basic-engineer/whileloop.js
@@ -9,7 +9,7 @@ let condition = true
     }
 //}
 
-function countDown(n) {
+//function countDown(n) {
     while (true) {
         console.log(`n: ${n}`)
         n = n - 1
@@ -18,8 +18,56 @@ function countDown(n) {
         }
     }
 console.log("countdown done")
+//}
+
+
+//console.log("countdown done")
+
+function promptStudent() {
+    let num1 = null;
+    let num2 = null;
+    while (true) {
+        num1 = prompt("Enter a number: ");
+        if (isNaN(num1)) {
+            countinue;
+        }
+        num2 = prompt("Enter another number: ");
+        if (isNaN(num2)) {
+            countinue;
+        }
+        break;
+    }
+    console.log(`Got numbers: ${num1} and ${num2}`);
+    mathTable(num1, num2);
 }
 
+function mathTable(num1, num2) {
+    if (!num1 || num1 < 0 || !num2 || num2 < 0) {
+        console.error("Invalid input. Please enter positive numbers.");
+        return;
+    }
+    let outerLoop = num1;
+    while (outerLoop > 0) {
+        doInnerLoop(outerLoop, num2);
+        outerLoop--;
+    }
+    //while(outerLoop > 0) {
+    //  let innerLoop = num2;
+    //  while (innerLoop > 0) {
+    //     console.log(`outerLoop: ${outerLoop}, innerLoop: ${innerLoop}`);
+    //     console.log (`${outerLoop} * ${innerLoop} = ${outerLoop * innerLoop}`)
+    //     innerLoop = innerLoop - 1;
+    //    }
+    //     outerLoop = outerLoop - 1
+    // }
+}
 
-console.log("countdown done")
-
+//individual//testseparte
+function doinnerLoop (outerLoop,num1) {
+    while (innerLoop > 0) {
+         console.log(`outerLoop: ${outerLoop}, innerLoop: ${innerLoop}`);
+         console.log (`${outerLoop} * ${innerLoop} = ${outerLoop * innerLoop}`)
+        innerLoop = innerLoop - 1;
+        }
+        innerLoop = innerLoop - 1;
+    }
