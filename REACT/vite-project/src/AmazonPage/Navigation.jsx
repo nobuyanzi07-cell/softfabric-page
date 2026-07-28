9934ed95adf7324408e45f54bf9531198f466a58
Nathan Oketch <nobuyanzi07@gmail.com>
2026-07-28 15:17:33 +0300

    start of nav section

---
+++ REACT/vite-project/src/AmazonPage/Navigation.jsx
@@ -0,0 +1,11 @@
+import logo from "./assets/amazon.png"
+
+function Navigation (){
+    return (
+        <div>
+            <h2> Navigation section</h2>
+        </div>
+    )
+}
+
+export default Navigation
--- REACT/vite-project/src/main.jsx
+++ REACT/vite-project/src/main.jsx
@@ -1,44 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-// Component that returns null
-function MyComponent() {
-  return null;
-}
-
-// Component that returns a Html element
-function MyComponent2(){
-  return (
-    <div>
-      <h1> fav text </h1>
-    </div>
-  );
-}
-
-// Component using a react fragment with multiple html elements
-function MyComponent3(){
-  return(
-    <>
-      <div></div>
-      <div></div>
-    </>
-  );
-}
-
-//Parent component rendering
-function ParentComponent(){
-  return (
-    <>
-      <MyComponent />
-      <MyComponent2></MyComponent2>
-      <MyComponent3 />
-    </>
-  );
-}
+import Navigation from "./AmazonPage/Navigation"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1> My first react app</h1>
-    <ParentComponent />
+   <Navigation/>
   </StrictMode>,
 );
